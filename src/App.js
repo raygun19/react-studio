c2baa68004b310ae05214ba207c01a935695edd0
9308233900 <71292939+raygun19@users.noreply.github.com>
2022-11-13 00:28:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,85 @@
-import "./App.css";
-import { useState } from "react";
-import bakeryData from "./assets/bakery-data.json";
+import React from 'react';
+// import './style.css';
+import CartList from './components/CartList';
+import { useState } from 'react';
+import bakeryData from './assets/bakery-data.json';
+import BakeryItem from './components/BakeryItem';
+
+import './App.css';
+
+
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/" + item.image;
 });
+
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+  const [m, setM] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
 
+  const addM = (txt, price) => {
+    //event.preventDefault();
+
+    let number = 1;
+
+    if (m.some((item) => item.text === txt)) {
+      let mm = [...m];
+      const a = mm.find((a) => a.text === txt);
+      a.num = a.num + 1;
+      setM(mm);
+    } else {
+      setM([
+        ...m,
+        {
+          num: number,
+          text: txt,
+          key: Date.now(),
+        },
+      ]);
+    }
+
+    let t = Math.round((total + price) * 100) / 100;
+    setTotal(t);
+  };
+
+  const removeM = (key) => {
+    // setM(m.filter((m) => key != m.key));
+  };
+  
   return (
-    <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+    <div id="main">
+      <div id="left">
+        <h1>My Bakery</h1>
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
+        <div id="items">
+          {bakeryData.map(
+            (
+              item,
+              index // TODO: map bakeryData to BakeryItem components
+            ) => (
+              <BakeryItem
+                addM={addM}
+                key={item.name}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+                index={index}
+              ></BakeryItem>
+            )
+          )}
+        </div>
+      </div>
+      <div id="right">
         <h2>Cart</h2>
         {/* TODO: render a list of items in the cart */}
+        <CartList m={m} remove={removeM} />
+        <p>Total: {total}</p>
       </div>
     </div>
   );
-}
-
+};
 export default App;
